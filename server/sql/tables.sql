74051cd6a9a992a5b5368cff6e813e564730dd28
TeaTracer <teatracer7@yandex.ru>
2017-02-17 22:09:46 +0300

    Fix database schema. (4)

--- server/sql/tables.sql
+++ server/sql/tables.sql
@@ -1,13 +1,9 @@
 REVOKE ALL ON SCHEMA public FROM public;
-
 CREATE SCHEMA aio;
 
 CREATE TABLE aio.local_managers (
 	id                   serial  NOT NULL,
-	first_name           varchar(100)  NOT NULL,
-	second_name          varchar(100)  NOT NULL,
-	hired_at             date  NOT NULL,
-	fired_at             date  ,
+	name                 varchar(100)  NOT NULL,
 	CONSTRAINT pk_local_managers PRIMARY KEY ( id )
  );
 
@@ -41,33 +37,33 @@ CREATE INDEX idx_categories ON aio.categories ( changed_by );
 
 CREATE TABLE aio.dishes (
 	id                   serial  NOT NULL,
-	init_id              integer  NOT NULL,
-	previous_id          integer  ,
+	init                 integer  NOT NULL,
+	previous             integer  ,
 	name                 varchar(100)  NOT NULL,
 	discription          text  NOT NULL,
 	price                numeric(10,2)  NOT NULL,
-	category_id          integer  NOT NULL,
-	tree_id              integer  NOT NULL,
+	category             integer  NOT NULL,
+	tree                 integer  NOT NULL,
 	changed_at           timestamp DEFAULT current_timestamp NOT NULL,
 	changed_by           integer  NOT NULL,
 	CONSTRAINT pk_dishes PRIMARY KEY ( id ),
 	CONSTRAINT fk_dishes FOREIGN KEY ( changed_by ) REFERENCES aio.local_managers( id )    ,
-	CONSTRAINT fk_dishes_0 FOREIGN KEY ( category_id ) REFERENCES aio.categories( id )    ,
-	CONSTRAINT fk_dishes_1 FOREIGN KEY ( tree_id ) REFERENCES aio.trees( id )
+	CONSTRAINT fk_dishes_0 FOREIGN KEY ( category ) REFERENCES aio.categories( id )    ,
+	CONSTRAINT fk_dishes_1 FOREIGN KEY ( tree ) REFERENCES aio.trees( id )
  );
 
 CREATE INDEX idx_dishes ON aio.dishes ( changed_by );
 
-CREATE INDEX idx_dishes_0 ON aio.dishes ( category_id );
+CREATE INDEX idx_dishes_0 ON aio.dishes ( category );
 
-CREATE INDEX idx_dishes_1 ON aio.dishes ( tree_id );
+CREATE INDEX idx_dishes_1 ON aio.dishes ( tree );
 
 CREATE TABLE aio.menu (
-	dish_id              integer NOT NULL,
+	dish                 integer  NOT NULL,
 	inserted_at          timestamp  NOT NULL,
 	inserted_by          integer  NOT NULL,
-	CONSTRAINT pk_menu PRIMARY KEY ( dish_id ),
-	CONSTRAINT fk_menu FOREIGN KEY ( dish_id ) REFERENCES aio.dishes( id )    ,
+	CONSTRAINT pk_menu PRIMARY KEY ( dish ),
+	CONSTRAINT fk_menu FOREIGN KEY ( dish ) REFERENCES aio.dishes( id )    ,
 	CONSTRAINT fk_menu_0 FOREIGN KEY ( inserted_by ) REFERENCES aio.local_managers( id )
  );
 
@@ -75,23 +71,20 @@ CREATE INDEX idx_menu ON aio.menu ( inserted_by );
 
 CREATE TABLE aio.remote_managers (
 	id                   serial  NOT NULL,
-	first_name           varchar(100)  NOT NULL,
-	second_name          varchar(100)  NOT NULL,
-	hired_at             date  NOT NULL,
-	fired_at             date  ,
-	restaurant_id        integer  NOT NULL,
+	name                 varchar(100)  NOT NULL,
+	restaurant           integer  NOT NULL,
 	CONSTRAINT pk_remote_managers PRIMARY KEY ( id ),
-	CONSTRAINT fk_remote_managers FOREIGN KEY ( restaurant_id ) REFERENCES aio.restaurants( id )
+	CONSTRAINT fk_remote_managers FOREIGN KEY ( restaurant ) REFERENCES aio.restaurants( id )
  );
 
-CREATE INDEX idx_remote_managers ON aio.remote_managers ( restaurant_id );
+CREATE INDEX idx_remote_managers ON aio.remote_managers ( restaurant );
 
 CREATE TABLE aio.orders (
 	id                   serial  NOT NULL,
 	manager              integer  NOT NULL,
 	tree                 integer  NOT NULL,
 	ordered_at           timestamp  NOT NULL,
-	order_content        json  NOT NULL,
+	content              json  NOT NULL,
 	CONSTRAINT pk_orders PRIMARY KEY ( id ),
 	CONSTRAINT fk_orders FOREIGN KEY ( manager ) REFERENCES aio.remote_managers( id )    ,
 	CONSTRAINT fk_orders_0 FOREIGN KEY ( tree ) REFERENCES aio.trees( id )
